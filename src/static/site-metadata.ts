9a453238ff7b901555f6518487cd827b4fb87904
LemonNoCry <ningmengbuku@outlook.com>
2024-03-13 15:33:12 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'Lemon Running Page',
+  siteUrl: 'https://github.com/LemonNoCry',
+  logo: 'https://i.imgur.com/w7hsPVb.jpeg',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'Github',
+      url: 'https://github.com/LemonNoCry',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://lemonnocry.github.io/running_page/',
     },
   ],
 };
